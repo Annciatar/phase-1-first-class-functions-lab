78ddf3cc84d65182ae68593ea64f59a2f8f5ccd0
Annciatar Ndumi <annciatar.kavita@student.moringaschool.com>
2022-08-17 20:17:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(){
+    return [...drivers].slice(0,2);
+
+}
+const returnLastTwoDrivers = function(){
+    return [...drivers].slice(2);
+}
+let selectingDrivers = []
+selectingDrivers.unshift(returnFirstTwoDrivers);
+selectingDrivers.push(returnLastTwoDrivers);
+ 
+const createFareMultiplier = function(num){
+    return function(){
+        return num*5;
+    }
+}
+const fareDoubler = function() {
+    return function(num){
+        return num*2;
+    }(10)
+}
+const fareTripler = function() {
+    return function(num) {
+        return num*3;
+    }(12)
+}
+const selectDifferentDrivers = function (arrayOfDrivers, func) {
+    return func(Math.random(arrayOfDrivers));
+}
